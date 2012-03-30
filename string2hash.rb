f286594c22ce15396cd41d0fc02d0d2a0fa3fde4
Max Spransy <maxjustus@gmail.com>
2012-03-30 09:48:32 -0500

    initial semi-pathetic commit

---
+++ string2hash.rb
@@ -0,0 +1,14 @@
+# Given this array of strings
+# use whatever you would consider to be most correct/clean/rad
+# to output a hash where the integer portion of each string is a key,
+# with it's corresponding name as the value.
+# prove it's correctness using a simple test.
+#
+# EXAMPLE
+#
+# Given
+#   ['herp_11', 'derp_31']
+# Expected output should be
+#   { 11 => 'herp', 31 => 'derp' }
+
+names_with_id = %w{steve_111 matt_53 dave_43 craig_112 erwin_46 larry_33 reggie_224 carl_32 nathan_11}
